6179679ad6ea22fafe78ffac6bcacb023e1ffd5c
vangiaurecca <vangiau.recca@gmail.com>
2020-12-20 22:53:42 +0700

    Update archievement js

---
+++ js/hieu_ung_so.js
@@ -0,0 +1,34 @@
+function animateNumber(finalNumber, delay, startNumber = 0, callback) {
+    let currentNumber = startNumber
+    const interval = window.setInterval(updateNumber, delay)
+    function updateNumber() {
+      if (currentNumber >= finalNumber) {
+        clearInterval(interval)
+      } else {
+        currentNumber++
+        callback(currentNumber)
+      }
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    animateNumber(20, 290, 0, function (number) {
+      const formattedNumber = number.toLocaleString()
+      document.getElementById('worked-count').innerText = formattedNumber
+    })
+    
+    animateNumber(2, 2000, 0, function (number) {
+      const formattedNumber = number.toLocaleString()
+      document.getElementById('city-count').innerText = formattedNumber
+    })
+    
+    animateNumber(50, 99, 0, function (number) {
+      const formattedNumber = number.toLocaleString()
+      document.getElementById('customer-count').innerText = formattedNumber
+    })
+
+    animateNumber(6, 1100, 0, function (number) {
+        const formattedNumber = number.toLocaleString()
+        document.getElementById('total-count').innerText = formattedNumber
+    })
+  })
