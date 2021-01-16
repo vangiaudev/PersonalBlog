57c5f87a53b4dd249500d8bc96f9debd4fc3d36b
vangiaurecca <vangiau.recca@gmail.com>
2021-01-16 16:08:51 +0700

    Update Sroll-Page

---
+++ js/scroll_page.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$(window).scroll(function(event) {
+		var pos_body = $('html,body').scrollTop();
+        if(pos_body > 3){
+            $('.header').addClass('sticky');
+            $('.text-menu').addClass('color-text-menu');
+            $('.menu-header').removeClass('list-menu');
+            $('.un-sticky').removeClass('un-sticky');
+            
+            
+        }
+        else {         
+            $('.header').removeClass('sticky');
+            $('.text-menu').removeClass('color-text-menu');
+            $('.menu-header').addClass('list-menu');
+            $('.header').addClass('un-sticky');
+            $('.header').removeClass('header-scroll');
+            
+        }
+        if(pos_body > 500){
+            $('.line-miss').addClass('line-miss-scroll');
+        }
+    });
+    $('.back-to-top').click(function(event){
+        $('html,body').animate({scrollTop:0}, 1400);
+    });
+    
+});
